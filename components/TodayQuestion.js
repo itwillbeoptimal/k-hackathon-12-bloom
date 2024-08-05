e6fb92a11c7f6395a6308d56b7b8f1d74c8a6799
itwillbeoptimal <itwillbeoptimal@gmail.com>
2024-08-05 19:49:55 +0900

    오늘의 질문 및 명언 컴포넌트 UI 수정

--- components/TodayQuestion.js
+++ components/TodayQuestion.js
@@ -1,51 +1,66 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import styled from "styled-components/native";
-import { Text, TouchableOpacity, View, ScrollView } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import WriteIcon from "../assets/buttons/write.svg";
 
 const TodayQuestionContainer = styled.View`
     flex-direction: column;
     margin: 16px;
     width: 280px;
-    height: 200px;
+    height: 230px;
     background-color: #E0EFFF;
     border-radius: 20px;
 `;
 
-const WriteButton = ({ onPress }) => (<TouchableOpacity onPress={onPress}>
-    <View style={{ marginRight: 8, marginTop: 25, alignSelf: "flex-end" }}>
+const WriteButton = ({ onPress }) => (
+  <TouchableOpacity onPress={onPress}>
+    <View style={{ marginRight: 8, alignSelf: "flex-end" }}>
       <WriteIcon />
     </View>
-  </TouchableOpacity>);
+  </TouchableOpacity>
+);
 
-const TodayQuestion = ({ questionDetail }) => (<View>
-    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+const TodayQuestion = ({ questionDetail }) => {
+  const [textHeight, setTextHeight] = useState(0);
+
+  // Text 컴포넌트의 높이를 측정하는 함수
+  const onTextLayout = useCallback((event) => {
+    const { height } = event.nativeEvent.layout;
+    setTextHeight(height);
+  }, []);
+
+  const isSingleLine = textHeight < 30;
+
+  return (
+    <View>
       <TodayQuestionContainer>
         <View style={{ padding: 25 }}>
           <Text style={{
             fontFamily: "SpoqaHanSansNeo-Medium",
             fontSize: 15,
             color: "#5494DA",
-            letterSpacing: -0.3,
-            alignSelf: "flex-start",
-          }}>오늘의
-            질문</Text>
-          <Text style={{
-            fontFamily: "GowunDodum-Regular",
-            fontSize: 18,
-            color: "#585A64",
-            textAlign: "center",
-            lineHeight: 25,
             letterSpacing: -1,
-            marginTop: 25,
-            alignSelf: "center",
-            height: 52
-          }}>{questionDetail}</Text>
+            alignSelf: "flex-start",
+          }}>오늘의 질문</Text>
+          <View style={{display: "flex", height: 130, padding: 5, justifyContent: "center"}}>
+            <Text
+              style={{
+                fontFamily: "GowunDodum-Regular",
+                fontSize: 18,
+                color: "#585A64",
+                textAlign: isSingleLine ? "center" : "justify", // 조건에 따라 정렬 변경
+                letterSpacing: -1.5,
+              }}
+              onLayout={onTextLayout}
+            >
+              {questionDetail}
+            </Text>
+          </View>
           <WriteButton />
         </View>
       </TodayQuestionContainer>
-      <TodayQuestionContainer />
-    </ScrollView>
-  </View>);
+    </View>
+  );
+}
 
 export default TodayQuestion;
